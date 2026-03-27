ed169ed525412b31ae4464b8410a46680e1d44b4
Ian Mark J. Buenaflor <ianmarkbuenaflor9@gmail.com>
2026-03-27 16:38:05 +0800

    Added About me features and i made some few changes on the SectionHeader components to improve flexibility

--- app/(guest)/about/page.tsx
+++ app/(guest)/about/page.tsx
@@ -1,7 +1,8 @@
+import { AboutPage } from "@/components/features/About/AboutPage"
 export default function About() {
   return (
-    <div className="container mx-auto">
-      <h1>About Page</h1>
+    <div>
+      <AboutPage />
     </div>
   )
 }
---
+++ components/features/About/AboutPage.tsx
@@ -0,0 +1,40 @@
+import { Section } from "@/components/common/Section"
+import Image from "next/image"
+import { SectionHeader } from "@/components/common/SectionHeader"
+export function AboutPage() {
+  return (
+    <Section id="AboutPage">
+      <div className="container mx-auto flex items-center justify-center rounded-2xl border-y bg-gray-100 p-10 dark:bg-card">
+        <div>
+          <h1 className="mb-5 text-3xl font-bold">ABOUT ME</h1>
+          <SectionHeader title="PASSIONATE ABOUT" />
+          <SectionHeader title="IT & DEVELOPMENT" className="text-blue-600" />
+          <div className="flex w-[85%] flex-col gap-5">
+            <p>
+              I am lan Mark Buenaflor, a third-year Bachelor of Science in
+              Information Technology student at CPC. I am currently deeply
+              immersed in developing my knowledge and skills in the field of web
+              application development.
+            </p>
+            <p>
+              I am committed to gaining practical experience, improving my
+              technical competencies, and expanding my understanding of modern
+              web technologies. As I continue my academic journey, I aim to
+              contribute to meaningful projects and further refine my abilities
+              as a future IT professional.
+            </p>
+          </div>
+        </div>
+        <div className="flex size-[75%] items-center justify-center">
+          <Image
+            src="/profile.jpg"
+            width={800}
+            height={800}
+            alt="Profile"
+            className="w-full rounded-lg transition-transform hover:scale-105 hover:rotate-1 hover:ring-3 hover:ring-blue-600"
+          />
+        </div>
+      </div>
+    </Section>
+  )
+}
